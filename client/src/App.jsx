7a3640a5ec68bfbc6896c13ca85d841cd92d7823
Asif12as <mda957947@gmail.com>
2023-09-11 23:59:02 +0530

    hello first

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import FileUpload from './FileUpload'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+    <h1>MINT YOUR NFT HERE !</h1>
+    <FileUpload></FileUpload>
+    </div>
+  )
+}
+
+export default App
